050a3483fd567ed64c3354d853150a16130ba945
Smaran Harihar <sharihar@esri.com>
2021-07-13 11:50:01 -0700

    add toggle and delete tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from "@angular/core";
-import { Observable, of } from "rxjs";
-import { TASKS } from "../mock-tasks";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
 import { Task } from "../Task";
+
+const headerOptions = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
 
 @Injectable({
   providedIn: "root"
 })
+
 export class TaskService {
+
+  apiUrl = "http://localhost:5000/tasks";
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<void> {
+    const deleteApiUrl = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<void>(deleteApiUrl);
+  }
+
+  updateToggleReminder(task: Task): Observable<void> {
+    const updateApiUrl = `${this.apiUrl}/${task.id}`;
+    return this.http.put<void>(updateApiUrl, task, headerOptions);
   }
 }
